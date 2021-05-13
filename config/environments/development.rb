b1aaafece1701332817f921a2f2b2aa3831720e0
cristina.marcela.lazar@gmail.com <johndoe@Johns-MacBook-Pro.local>
2021-05-13 10:45:49 +0300

    broadcast updates with Action Cable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,10 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # for development with multiple machines, disbale the default check where Rails
+  # only allows access from the localhost when running in developmentmode
+  config.action_cable.disable_request_forgery_protection = true
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
