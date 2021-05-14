b2e011e921b1e490c61d4dcfb64cc0bba4b55beb
cristina.marcela.lazar@gmail.com <johndoe@Johns-MacBook-Pro.local>
2021-05-14 16:11:19 +0300

    order capture form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+LineItem.delete_all
 Product.delete_all
 #
 Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
@@ -9,4 +10,14 @@ Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
       </p>},
   image_url: '7apps.jpg',
   price: 30.00)
+
+Product.create!(title: 'Seven Mobile Apps in Seven Weeks Second Edition',
+  description:
+    %{<p>
+      <em>Native Apps, Multiple Platforms</em>
+      Answer the question "Can we build this for ALL the devices?"
+
+      </p>},
+  image_url: '7apps.jpg',
+  price: 40.00)
 #
